e531638ed2504a1b72d63a3a0938c79757f9ccb1
Ankit <ankitspare981@gmail.com>
2026-02-17 21:29:56 +0530

    Backend server+user auth+db connection

---
+++ server/controllers/AuthControllers.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from "express";
+import bcrypt from 'bcrypt'
+import User from "../models/user.js";
+import { error } from "node:console";
+
+// controllers for user registration
+
+export const registerUser = async(req: Request, res: Response)=>{
+    try {
+        const {name, email, password} = req.body;
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({message: 'User already exists'})
+        }
+        // password encryption
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const newUser=new User({name,email,password:hashedPassword})
+        await newUser.save()
+        // setting user data in session
+        req.session.isLoggedIn=true;
+        req.session.userId=newUser._id;
+        return res.json({
+            message: 'Account created successfully',
+            user: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            }
+        })
+    } catch (error:any) {
+        console.log(error)
+        res.status(500).json({message: error.message})
+    }
+}
+
+export const loginUser = async(req: Request, res: Response)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message: 'Invalid Email or Password'})
+        }
+        // password encryption
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: 'Invalid Email or Password'})
+        }
+        // setting user data in session
+        req.session.isLoggedIn=true;
+        req.session.userId=user._id;
+        return res.json({
+            message: 'Logged in successfully',
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    } catch (error:any) {
+        console.log(error);
+        res.status(500).json({message: error.message})
+    }
+}
+
+// controllers for user logout
+export const logoutUser = async(req: Request, res: Response)=>{
+    req.session.destroy((error:any)=>{
+        if(error){
+            console.log(error)
+            return res.status(500).json({message:error.message})
+        }
+    })
+    return res.json({message: 'LogOut Successful'})
+}
+
+// controllers for user verify
+export const verifyUser = async(req: Request, res: Response)=>{
+    try {
+        const {userId} = req.session;
+        const user = await User.findById(userId).select('-password')
+        if(!user){
+            return res.status(400).json({message: 'Invalid user'})
+        }
+        return res.json({user})
+    } catch (error:any) {
+        console.log(error);
+        res.status(500).json({message: error.message})
+    }
+}
---
+++ server/server.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import session from 'express-session';
+import MongoStore from 'connect-mongo'
+import AuthRouter from "./routes/AuthRouters.js";
+declare module 'express-session'{
+    interface SessionData{
+        isLoggedIn: boolean;
+        userId: string
+    }
+}
+await connectDB()
+
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin: ['http://localhost:5173','http://localhost:3000'],
+    credentials: true
+}))
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge:1000*60*60*24*7}, //7 days expiry
+    store: MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}))
+app.use(express.json());
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+app.use('/api/auth',AuthRouter)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
