95e6c731bc15946dc31bb0efcc2deeac4cee05d8
Ankit <ankitspare981@gmail.com>
2026-02-17 17:20:52 +0530

    Frontend done

--- client/reactjs/src/pages/Generate.tsx
+++ client/reactjs/src/pages/Generate.tsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import { IThumbnail } from "../assets/assets";
+import { type AspectRatio, colorSchemes, type ThumbnailStyle, type IThumbnail, dummyThumbnails } from "../assets/assets";
+import SoftBackdrop from "../components/SoftBackdrop";
+import AspectRatioSelector from "../components/AspectRatioSelector";
+import StyleSelector from "../components/StyleSelector";
+import ColorSchemeSelector from "../components/ColorSchemeSelector";
+import PreviewPanel from "../components/PreviewPanel";
 
 
 const Generate = () => {
@@ -9,9 +14,85 @@ const Generate = () => {
     const [addDetails,setAddDetails]=useState('')
     const [thumbnail,setThumbnail]=useState<IThumbnail|null>(null)
     const [loading, setLoading]=useState(false)
+    const [aspectRatio,setAspectRatio]=useState<AspectRatio>('16:9')
+    const[colorSchemeId,setColorSchemId]=useState<string>(colorSchemes[0].id)
+    const[style,setStyle]=useState<ThumbnailStyle>('Bold & Graphic')
+    const[styleDropDownOpen,setStyleDropDownOpen]=useState(false)
+    const handleGenerate=async()=>{
+
+    }
+
+    const fetchThumbnail = async ()=>{
+        if(id){
+            const thumbnail:any=dummyThumbnails.find((thumbnail)=>thumbnail._id===id);
+            setThumbnail(thumbnail)
+            setAddDetails(thumbnail.user_prompt)
+            setTitle(thumbnail.title)
+            setColorSchemId(thumbnail.color_scheme)
+            setAspectRatio(thumbnail.aspect_ratio)
+            setStyle(thumbnail.style)
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        if(id){
+            fetchThumbnail()
+        }
+    },[id])
 
   return (
-    <div>Generate</div>
+    <>
+        <SoftBackdrop/>
+        <div className="pt-24 min-h-screen">
+            <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8">
+                <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+                    {/* LEFT panel */}
+                    <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+                        <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+                            <div>
+                                <h2 className="text-xl font-bold text-zinc-100">Create your Thumbnail</h2>
+                                <p className="text-sm text-zinc-100">Describe your vision and let AI bring it to life</p>
+                            </div>
+                            <div className="space-y-5">
+                                {/* Title input */}
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium">Title or Topic</label>
+                                    <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} maxLength={100} placeholder="e.g., 10 Tips for Better Sleep" className="w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500"/>
+                                    <div className="flex justify-end">
+                                        <span className="text-xs text-zinc-400">{title.length}/100</span>
+                                    </div>
+                                </div>
+                                {/* aspect ratio selector */}
+                                <AspectRatioSelector value={aspectRatio} onChange={setAspectRatio}/>
+                                {/* style selector */}
+                                <StyleSelector value={style} onChange={setStyle} isOpen={styleDropDownOpen} setIsOpen={setStyleDropDownOpen}/>
+                                {/* color scheme selector */}
+                                <ColorSchemeSelector value={colorSchemeId} onChange={setColorSchemId}/>
+                                {/* details */}
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium">Additional Prompts <span className="text-zinc-400 text-xs">(optional)</span>
+                                    </label>
+                                    <textarea value={addDetails} onChange={(e)=>setAddDetails(e.target.value)} rows={3} placeholder="Add any specific elements, moods, or style preferences..." className="w-full px-4 py-3 rounded-lg border border-white/10 bg-white/6 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-pink-500 resize-none"/>
+                                </div>
+                            </div>
+                            {/* BUTTON */}
+                            {!id && (<button onClick={handleGenerate} className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
+                                {loading?'Generating....':'Generate Thumbnail'}
+                            </button>)}
+                        </div>
+                    </div>
+                    {/* RIGHT panel */}
+                    <div>
+                        <div className="p-6 rounded-2xl bg-white/8 border border-white/10 shadow-xl">
+                            <h2 className="text-lg font-semibold text-zinc-100 mb-4">Preview</h2>
+                            <PreviewPanel thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio}/>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </>
   )
 }
 
